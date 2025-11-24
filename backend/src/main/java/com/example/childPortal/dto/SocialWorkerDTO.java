6bfe23574d4a86e1577a43b335d1879a241df336
AmashaFernando <amashanilothpalif@gmail.com>
2025-11-24 14:38:39 +0530

    Add SocialWorkerDTO.java

---
+++ backend/src/main/java/com/example/childPortal/dto/SocialWorkerDTO.java
@@ -0,0 +1,56 @@
+package com.example.childPortal.dto;
+
+import java.util.List;
+
+public class SocialWorkerDTO {
+    private String userId;
+    private String licenseNumber;
+    private List<String> specializations;
+    private String organization;
+    private String yearsOfExperience;
+    private String certificationUrl;
+
+    public SocialWorkerDTO() {}
+
+    public String getUserId() { 
+        return userId; 
+    }
+    public void setUserId(String userId) { 
+        this.userId = userId; 
+    }
+
+    public String getLicenseNumber() { 
+        return licenseNumber; 
+    }
+    public void setLicenseNumber(String licenseNumber) { 
+        this.licenseNumber = licenseNumber; 
+    }
+
+    public List<String> getSpecializations() { 
+        return specializations; 
+    }
+    public void setSpecializations(List<String> specializations) { 
+        this.specializations = specializations; 
+    }
+
+    public String getOrganization() { 
+        return organization; 
+    }
+    public void setOrganization(String organization) { 
+        this.organization = organization; 
+    }
+
+    public String getYearsOfExperience() { 
+        return yearsOfExperience; 
+    }
+    public void setYearsOfExperience(String yearsOfExperience) { 
+        this.yearsOfExperience = yearsOfExperience; 
+    }
+
+    public String getCertificationUrl() { 
+        return certificationUrl; 
+    }
+    public void setCertificationUrl(String certificationUrl) { 
+        this.certificationUrl = certificationUrl; 
+    }
+}

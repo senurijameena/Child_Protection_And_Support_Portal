809fdbf2bb86fff9731a29a3191c2b9b31fb5b04
Janani <jananinethma570@gmail.com>
2025-11-25 17:34:50 +0530

    FIX CORS and Authorization

--- backend/src/main/java/com/example/childPortal/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/childPortal/security/JwtAuthenticationFilter.java
@@ -44,10 +44,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         
         header = request.getHeader("Authorization");
         
-        if (header != null && header.startsWith("Bearer "){
+        if (header != null && header.startsWith("Bearer ")){
             String token = header.substring(7);
             
-            if (jwtUtil.validateToken(token){
+            if (jwtUtil.validateToken(token)){
                 String email = jwtUtil.getEmailFromToken(token);
                 String role = jwtUtil.getRoleFromToken(token);
                 

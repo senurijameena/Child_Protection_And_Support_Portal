8a5db302d5e0becdb0d52a4974ec22aa27dbfa4e
senurijameena <senurijameena@gmail.com>
2025-12-27 10:59:21 +0530

    Update Notification.java

--- backend/src/main/java/com/example/childPortal/model/Notification.java
+++ backend/src/main/java/com/example/childPortal/model/Notification.java
@@ -1,1 +1,9 @@
+package com.example.childPortal.model;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document; 
+import java.time.LocalDateTime;
 
+@Document(collection = "notifications") 
+  public class Notification {
+    @Id
+    private String id;

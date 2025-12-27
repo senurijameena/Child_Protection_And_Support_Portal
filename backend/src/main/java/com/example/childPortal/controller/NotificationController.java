817f12a95e83a4715f7b518acab3d3bb8316462c
senurijameena <senurijameena@gmail.com>
2025-12-27 11:07:55 +0530

    Update NotificationController.java

--- backend/src/main/java/com/example/childPortal/controller/NotificationController.java
+++ backend/src/main/java/com/example/childPortal/controller/NotificationController.java
@@ -1,1 +1,8 @@
-
+package com.example.childPortal.controller;
+import com.example.childPortal.model.Notification;
+import com.example.childPortal.repository.NotificationRepository;
+import com.example.childPortal.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal; 
+import org.springframework.web.bind.annotation.*;

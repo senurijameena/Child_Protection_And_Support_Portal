296fe4f7a23898559110fa3a4ab0b1f6f8f96502
senurijameena <senurijameena@gmail.com>
2025-12-27 10:35:58 +0530

    Create  SearchController.java

---
+++ backend/src/main/java/com/example/childPortal/controller/SearchController.java
@@ -0,0 +1,9 @@
+package com.example.childPortal.controller;
+import com.example.childPortal.dto.*;
+import com.example.childPortal.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
